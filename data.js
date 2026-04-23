7d946154bc09395f5c8e9e88ac05fcd845c42ca7
designnotearchitects-lab <designnotearchitects@gmail.com>
2026-04-23 13:32:58 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hanging-stand",
+      "name": "HANGING STAND",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13582645304533614,
+          "pitch": 1.3514671900817294,
+          "rotation": 0,
+          "target": "0-hanging-stand"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-from-entry",
+      "name": "FROM ENTRY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6617187776712612,
+          "pitch": 1.2407936907866066,
+          "rotation": 0,
+          "target": "1-from-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bangles",
+      "name": "BANGLES",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3507605609609943,
+          "pitch": 1.0743655985874074,
+          "rotation": 0,
+          "target": "2-bangles"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-rentals",
+      "name": "RENTALS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3690484586137295,
+          "pitch": 0.7949166587808438,
+          "rotation": 0,
+          "target": "3-rentals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-office",
+      "name": "OFFICE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.202567046728781,
+          "pitch": 0.4686707314231029,
+          "rotation": 0,
+          "target": "4-office"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "AMELIA BOUTIQUE 3D RENDER",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
